66ef1f9475b18c436d9e85d4b74e1cfc5ee6349c
508LoopDetected <themilstead@gmail.com>
2023-05-09 18:38:29 -0500

    metadata update

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ axios.get(`http://${process.env.PLEX_SERVER_ADDR}:32400/status/sessions?X-Plex-T
   .then(response => {
     const sessions = response.data.MediaContainer.Metadata;
     if (sessions && sessions.length > 0) {
-      const currentSong = sessions[0].title;
-      console.log(`Currently playing song: ${currentSong}`);
+      const currentSong = sessions[0];
+      const albumArt = `http://${process.env.PLEX_SERVER_ADDR}:32400${currentSong.thumb}?X-Plex-Token=${process.env.PLEX_AUTH_TOKEN}&minSize=1`;
+      const songTitle = currentSong.title;
+      const artist = currentSong.grandparentTitle;
+
+      console.log(albumArt);
+      console.log(artist);
+      console.log(songTitle);
+
     } else {
       console.log('No sessions found');
     }
